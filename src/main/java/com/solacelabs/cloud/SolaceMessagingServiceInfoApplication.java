20cfc24477263a2272c52570374976bac229120c
mdspielman <mark@spielman.ca>
2016-07-18 10:12:09 -0400

    Add Spring starter template with Gradle build

---
+++ src/main/java/com/solacelabs/cloud/SolaceMessagingServiceInfoApplication.java
@@ -0,0 +1,12 @@
+package com.solacelabs.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolaceMessagingServiceInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolaceMessagingServiceInfoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/solacelabs/cloud/SolaceMessagingServiceInfoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.solacelabs.cloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SolaceMessagingServiceInfoApplication.class)
+public class SolaceMessagingServiceInfoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
