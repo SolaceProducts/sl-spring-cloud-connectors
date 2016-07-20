5f90c0ebbc85eefc994ca220a7cf06cc582582f6
mdspielman <mark@spielman.ca>
2016-07-20 14:31:20 -0400

    A simple build to package code as a jar. Works with prototype. No tests yet.

--- src/main/java/com/solacesystems/labs/cloud/spring/cloudfoundry/SolaceMessagingInfoCreator.java
+++ src/main/java/com/solacesystems/labs/cloud/spring/cloudfoundry/SolaceMessagingInfoCreator.java
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package com.solacesystems.labs.cloud;
+package com.solacesystems.labs.cloud.spring.cloudfoundry;
 
 import java.util.Map;
 
---
+++ src/test/java/com/solacesystems/labs/cloud/spring/cloudfoundry/SolaceMessagingServiceInfoTests.java
@@ -0,0 +1,12 @@
+package com.solacesystems.labs.cloud.spring.cloudfoundry;
+
+import org.junit.Test;
+
+
+public class SolaceMessagingServiceInfoTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
